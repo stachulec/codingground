e5a78d804d85b68bfb1a414f7e7d509c1e042a8d
kchodnicki <kchodnicki@gmail.com>
2015-08-03 11:53:50 +0200

    Created by Coding Ground Client

---
+++ Tetris AI/Bot/Strategies/RandomStrategy.py
@@ -0,0 +1,16 @@
+from random import randint
+
+from AbstractStrategy import AbstractStrategy
+
+
+class RandomStrategy(AbstractStrategy):
+    def __init__(self, game):
+        AbstractStrategy.__init__(self, game)
+        self._actions = ['left', 'right', 'turnleft', 'turnright', 'down', 'drop']
+
+    def choose(self):
+        ind = [randint(0, 4) for _ in range(1, 10)]
+        moves = map(lambda x: self._actions[x], ind)
+        moves.append('drop')
+
+        return moves
