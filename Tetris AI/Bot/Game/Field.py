d6d24efafd3e6e9fc1e4c392a692061e8c169a7b
kchodnicki <kchodnicki@gmail.com>
2015-08-03 11:44:48 +0200

    Created by Coding Ground Client

---
+++ Tetris AI/Bot/Game/Field.py
@@ -0,0 +1,59 @@
+import copy
+
+class Field:
+    def __init__(self):
+        self.width = 10
+        self.height = 20
+        self.field = [[0]*self.width]*self.height
+
+    def size(self):
+        return self.width, self.height
+
+    def updateField(self, field):
+        self.field = field
+
+    def projectPieceDown(self, piece, offset):
+        piecePositions = self.__offsetPiece(piece.positions(), offset)
+
+        field = None
+        for height in range(0, self.height-1):
+            tmp = self.fitPiece(piecePositions, [0, height])
+
+            if not tmp:
+                break
+            field = tmp
+
+        return field
+
+    @staticmethod
+    def __offsetPiece(piecePositions, offset):
+        piece = copy.deepcopy(piecePositions)
+        for pos in piece:
+            pos[0] += offset[0]
+            pos[1] += offset[1]
+
+        return piece
+
+    def __checkIfPieceFits(self, piecePositions):
+        for x,y in piecePositions:
+            if 0 <= x < self.width and 0 <= y < self.height:
+                if self.field[y][x] > 1:
+                    return False
+            else:
+                return False
+        return True
+
+    def fitPiece(self, piecePositions, offset=None):
+        if offset:
+            piece = self.__offsetPiece(piecePositions, offset)
+        else:
+            piece = piecePositions
+
+        field = copy.deepcopy(self.field)
+        if self.__checkIfPieceFits(piece):
+            for x,y in piece:
+                field[y][x] = 4
+
+            return field
+        else:
+            return None
