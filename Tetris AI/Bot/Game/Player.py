8fb6152237a430d1ddcf98aa71796416df5304c5
kchodnicki <kchodnicki@gmail.com>
2015-08-03 11:44:48 +0200

    Created by Coding Ground Client

---
+++ Tetris AI/Bot/Game/Player.py
@@ -0,0 +1,16 @@
+from Field import Field
+
+
+class Player:
+    def __init__(self, name=None):
+        self.name = name
+        self.rowPoints = 0
+        self.combo = 0
+
+        self.field = Field()
+
+    def updateRowPoints(self, rowPoints):
+        self.rowPoints = rowPoints
+
+    def updateCombo(self, combo):
+        self.combo = combo
